a5138cee6de8a07e0672705577e57041aac5dce0
Eisenkassette <daniel.larionov@protonmail.com>
2024-10-10 13:21:13 +0200

    Created tests, happy and sad path

---
+++ tests/unit_test/test_booking_above_twelve.py
@@ -0,0 +1,38 @@
+import pytest
+
+def test_booking_more_than_twelve(client, mocker, mock_clubs, mock_competitions):
+    mocker.patch('server.loadClubs', return_value=mock_clubs)
+    mocker.patch('server.loadCompetitions', return_value=mock_competitions)
+
+    loggedinclub_name = mock_clubs[0]['name']
+    competition_name = mock_competitions[0]['name']
+
+    places_to_book = 12 + 1
+
+    # POST request to the purchasePlaces endpoint
+    response = client.post('/purchasePlaces', data={
+        'club': loggedinclub_name,
+        'competition': competition_name,
+        'places': places_to_book
+    })
+
+    assert response.status_code == 403, f"Expected status code 403, but got {response.status_code}"
+
+
+def test_booking_less_than_twelve(client, mocker, mock_clubs, mock_competitions):
+    mocker.patch('server.loadClubs', return_value=mock_clubs)
+    mocker.patch('server.loadCompetitions', return_value=mock_competitions)
+
+    loggedinclub_name = mock_clubs[0]['name']
+    competition_name = mock_competitions[0]['name']
+
+    places_to_book = 5
+
+    # POST request to the purchasePlaces endpoint
+    response = client.post('/purchasePlaces', data={
+        'club': loggedinclub_name,
+        'competition': competition_name,
+        'places': places_to_book
+    })
+
+    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
