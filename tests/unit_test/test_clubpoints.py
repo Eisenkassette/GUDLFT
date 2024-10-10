240c3550d9cb43e1fffa7c94fe1edeaa9a204f8a
Eisenkassette <daniel.larionov@protonmail.com>
2024-10-10 13:03:35 +0200

    Created overbooking sad path test

---
+++ tests/unit_test/test_clubpoints.py
@@ -0,0 +1,19 @@
+import pytest
+
+def test_over_booking(client, mocker, mock_clubs, mock_competitions):
+    mocker.patch('server.loadClubs', return_value=mock_clubs)
+    mocker.patch('server.loadCompetitions', return_value=mock_competitions)
+
+    loggedinclub_name = mock_clubs[0]['name']
+    competition_name = mock_competitions[0]['name']
+
+    places_to_book = int(mock_clubs[0]['points']) + 1
+
+    # POST request to the purchasePlaces endpoint
+    response = client.post('/purchasePlaces', data={
+        'club': loggedinclub_name,
+        'competition': competition_name,
+        'places': places_to_book
+    })
+
+    assert response.status_code == 403, f"Expected status code 403, but got {response.status_code}, {places_to_book}"
