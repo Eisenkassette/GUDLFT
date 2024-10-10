ccdb566e128b70f1cb119bdb59591ca35e850f88
Eisenkassette <daniel.larionov@protonmail.com>
2024-10-10 15:46:08 +0200

    Refactored mock data

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,36 +7,16 @@ def client():
     with app.test_client() as client:
         yield client
 
-@pytest.fixture
-def mock_clubs():
-    return [
-        {
-            "name":"Simply Lift",
-            "email":"john@simplylift.co",
-            "points":"13"
-        },
-        {
-            "name":"Iron Temple",
-            "email": "admin@irontemple.com",
-            "points":"4"
-        },
-        {   "name":"She Lifts",
-            "email": "kate@shelifts.co.uk",
-            "points":"12"
-        }
+@pytest.fixture(autouse=True)
+def mock_data(mocker):
+    mock_clubs = [
+        {"name": "Simply Lift", "email": "john@simplylift.co", "points": "13"},
+        {"name": "Iron Temple", "email": "admin@irontemple.com", "points": "4"},
+        {"name": "She Lifts", "email": "kate@shelifts.co.uk", "points": "12"}
     ]
-
-@pytest.fixture
-def mock_competitions():
-    return [
-        {
-            "name": "Spring Festival",
-            "date": "2020-03-27 10:00:00",
-            "numberOfPlaces": "25"
-        },
-        {
-            "name": "Fall Classic",
-            "date": "2020-10-22 13:30:00",
-            "numberOfPlaces": "13"
-        }
+    mock_competitions = [
+        {"name": "Spring Festival", "date": "2030-03-27 10:00:00", "numberOfPlaces": "25"},
+        {"name": "Fall Classic", "date": "2020-10-22 13:30:00", "numberOfPlaces": "13"}
     ]
+    mocker.patch('server.clubs', mock_clubs)
+    mocker.patch('server.competitions', mock_competitions)
